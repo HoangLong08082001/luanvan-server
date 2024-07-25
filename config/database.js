15b386609c0f240c17994b0aee6cf4e282939a35
HoangLong08082001 <hahoanglong08082001@gmail.com>
2024-07-25 23:55:48 +0700

    deploy aggain

--- config/database.js
+++ config/database.js
@@ -1,5 +1,13 @@
 const mysql = require("mysql");
 
+// const pool = mysql.createConnection({
+//   port: 3306,
+//   host: "bdkrv4uf7kpaniqwj931-mysql.services.clever-cloud.com",
+//   user: "ukkswwf1deccs8x8",
+//   password: "nOIm9aunrqj9MavlqrQz",
+//   database: "bdkrv4uf7kpaniqwj931",
+//   connectionLimit: 10,
+// });
 const pool = mysql.createConnection({
   port: 3306,
   host: "localhost",

ba6e90d2ee29abecd196151d5bf62d5100c08e20
HoangLong08082001 <hahoanglong08082001@gmail.com>
2024-08-03 19:28:57 +0700

    commit again 3/8/2024

--- config/database.js
+++ config/database.js
@@ -1,21 +1,21 @@
 const mysql = require("mysql");
 
-const pool = mysql.createConnection({
-  port: 3306,
-  host: "bdkrv4uf7kpaniqwj931-mysql.services.clever-cloud.com",
-  user: "ukkswwf1deccs8x8",
-  password: "nOIm9aunrqj9MavlqrQz",
-  database: "bdkrv4uf7kpaniqwj931",
-  connectionLimit: 10,
-});
 // const pool = mysql.createConnection({
 //   port: 3306,
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "sportretal",
+//   host: "bdkrv4uf7kpaniqwj931-mysql.services.clever-cloud.com",
+//   user: "ukkswwf1deccs8x8",
+//   password: "nOIm9aunrqj9MavlqrQz",
+//   database: "bdkrv4uf7kpaniqwj931",
 //   connectionLimit: 10,
 // });
+const pool = mysql.createConnection({
+  port: 3306,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "sportretal",
+  connectionLimit: 10,
+});
 
 pool.connect((err) => {
   if (err) {
